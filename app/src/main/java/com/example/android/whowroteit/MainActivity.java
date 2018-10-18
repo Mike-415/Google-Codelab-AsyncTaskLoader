b52b911f9ce662c02540f901be7f183d13b562a9
Mike-415 <mgonz160@mail.ccsf.edu>
2018-10-17 17:07:17 -0700

    Implemented JSON parsing in the onPostExecute method

--- app/src/main/java/com/example/android/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/android/whowroteit/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,6 +53,40 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            try {
+                JSONObject jsonObect = new JSONObject(s);
+                JSONArray itemsArray = jsonObect.getJSONArray("items");
+                int i = 0;
+                String title = null;
+                String authors = null;
+                while(i < itemsArray.length() && (authors == null && title == null)){
+                    JSONObject book = itemsArray.getJSONObject(i);
+                    JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                    //Try to get the author and title from the current item,
+                    //catch if either field is empty and move on.
+                    try{
+                        title = volumeInfo.getString("title");
+                        authors = volumeInfo.getString("authors");
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                    i++;
+                }
+
+                if(title != null && authors != null){
+                    mTitleText.get().setText(title);
+                    mAuthorText.get().setText(authors);
+                } else {
+                    mTitleText.get().setText(R.string.no_results);
+                    mAuthorText.get().setText("");
+                }
+            } catch (JSONException e) {
+                // If onPostExecute does not receive a proper JSON string,
+                // update the UI to show failed results.
+                mTitleText.get().setText(R.string.no_results);
+                mAuthorText.get().setText("");
+                e.printStackTrace();
+            }
         }
     }
 }
